b8164ef6a8536d7e07c68bb608b428c29e2368eb
César Rodrigo <cramirezf@mitic.gov.py>
2024-06-16 21:50:35 -0400

    Add product and category

---
+++ src/main/java/com/ideacop/ecommerce/backend/domain/model/Category.java
@@ -0,0 +1,13 @@
+package com.ideacop.ecommerce.backend.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private Integer id;
+    private String name;
+}
--- src/main/java/com/ideacop/ecommerce/backend/domain/model/User.java
+++ src/main/java/com/ideacop/ecommerce/backend/domain/model/User.java
@@ -1,7 +1,14 @@
 package com.ideacop.ecommerce.backend.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Integer id;
     private String username;
